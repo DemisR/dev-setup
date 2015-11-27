a5c81b3fa497b1bcb07c70bf7be1b8fc1a6db58a
rizzotto <demis@exxoss.com>
2015-11-27 21:43:30 +0100

    My personal version

--- brew.sh
+++ brew.sh
@@ -1,173 +1,148 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+## Install command-line tools using Homebrew.
 
-# Ask for the administrator password upfront.
+## Ask for the administrator password upfront.
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
+## Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
+## Check for Homebrew,
+## Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Make sure we’re using the latest Homebrew.
+## Make sure we’re using the latest Homebrew.
 brew update
 
-# Upgrade any already-installed formulae.
+## Upgrade any already-installed formulae.
 brew upgrade --all
 
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install Bash 4.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-# We installed the new shell, now we have to activate it
-echo "Adding the newly installed shell to the list of allowed shells"
-# Prompts for password
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-# Change to the new shell, prompts for password
-chsh -s /usr/local/bin/bash
-
-# Install `wget` with IRI support.
+## Install `wget` with IRI support.
 brew install wget --with-iri
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
-# Install Python
+###############################################################################
+# Python                                                                      #
+###############################################################################
+## Install Python
 brew install python
-brew install python3
+#brew install python3
 
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
+# Install pip
+easy_install pip
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+# Install virtual environments globally
+pip install virtualenv
+pip install virtualenvwrapper
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+echo "------------------------------"
+echo "Source virtualenvwrapper from ~/.extra"
 
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
+EXTRA_PATH=~/.extra
+echo $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
+echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
+echo "" >> $BASH_PROFILE_PATH
+source $EXTRA_PATH
+###############################################################################
+
+## Install other useful binaries.
+#brew install dark-mode
 brew install git
 brew install git-lfs
 brew install git-flow
 brew install git-extras
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
 brew install ssh-copy-id
 brew install tree
-brew install webkit2png
-brew install zopfli
-brew install pkg-config libffi
+brew install nmap
+brew install httpie
+brew install csshx
+brew install whatmask
+
+## Install universal document converter
 brew install pandoc
 
-# Lxml and Libxslt
-brew install libxml2
-brew install libxslt
-brew link libxml2 --force
-brew link libxslt --force
-
-# Install Heroku
-brew install heroku-toolbelt
-heroku update
-
-# Install Cask
+## Install Cask
 brew install caskroom/cask/brew-cask
 
-# Core casks
-brew cask install --appdir="/Applications" alfred
-brew cask install --appdir="~/Applications" iterm2
-brew cask install --appdir="~/Applications" java
-brew cask install --appdir="~/Applications" xquartz
+## Core casks
+brew cask install --appdir="/Applications" iterm2
+#brew cask install --appdir="/Applications" java
 
-# Development tool casks
-brew cask install --appdir="/Applications" sublime-text3
-brew cask install --appdir="/Applications" atom
+## Development tool casks
 brew cask install --appdir="/Applications" virtualbox
 brew cask install --appdir="/Applications" vagrant
-brew cask install --appdir="/Applications" heroku-toolbelt
+brew cask install --appdir="/Applications" sourcetree
 brew cask install --appdir="/Applications" macdown
 
-# Misc casks
+## Install Atom code editor
+brew cask install --appdir="/Applications" atom
+## Install Atom plugins
+#apm install atom-beautify
+#apm install block-comment
+#apm install ever-notedown
+#apm install file-icons
+#apm install git-history
+#apm install git-log
+#apm install language-generic-config
+#apm install language-nagios
+#apm install linter
+#apm install linter-pylama
+#apm install linter-shellcheck
+#apm install merge-conflicts
+#apm install open-recent
+echo "Use this commands tu install most useful Atom plugins:
+
+#apm install atom-beautify
+#apm install block-comment
+#apm install ever-notedown
+#apm install file-icons
+#apm install git-history
+#apm install git-log
+#apm install language-generic-config
+#apm install language-nagios
+#apm install linter
+#apm install linter-pylama
+#apm install linter-shellcheck
+#apm install merge-conflicts
+#apm install open-recent
+"
+
+## Misc casks
 brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" skype
 brew cask install --appdir="/Applications" slack
-brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="/Applications" evernote
-#brew cask install --appdir="/Applications" gimp
-#brew cask install --appdir="/Applications" inkscape
 
-#Remove comment to install LaTeX distribution MacTeX
-#brew cask install --appdir="/Applications" mactex
+## Install Docker, which requires virtualbox
+#brew install docker
+#brew install boot2docker
 
-# Link cask apps to Alfred
-brew cask alfred link
+# Othe Applications
+brew cask install --appdir="/Applications" remote-desktop-connectio
+#brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" vlc
+#brew cask install --appdir="/Applications" transmission
+brew cask install --appdir="/Applications" caffeine
+#brew cask install --appdir="/Applications" valentina-studio
+brew cask install --appdir="/Applications" cheatsheet
+brew cask install --appdir="/Applications" filezilla
+# microsoft remote desktop
+brew cask install --appdir="/Applications" microsoft-office
 
-# Install Docker, which requires virtualbox
-brew install docker
-brew install boot2docker
+## Dev
+brew cask install --appdir="/Applications" sourcetree
+brew cask install --appdir="/Applications" intellij-idea
 
-# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
+## Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
-# Remove outdated versions from the cellar.
+## Remove outdated versions from the cellar.
 brew cleanup
